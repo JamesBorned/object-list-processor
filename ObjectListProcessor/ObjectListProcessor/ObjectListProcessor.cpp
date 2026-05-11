034d0e4691a79d8aa70f7c6e6bae1a1485a87a0d
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2026-05-11 22:46:02 +0300

    docs: add an object list file

---
+++ ObjectListProcessor/ObjectListProcessor/ObjectListProcessor.cpp
@@ -0,0 +1,9 @@
+﻿// ObjectListProcessor.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
